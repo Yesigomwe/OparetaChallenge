41f8f8fda3643df1d3504e0c8cf04bcf49990c26
Yesigomwe <yesigek@gmail.com>
2022-08-26 14:14:20 +0300

    Added API Key

--- app/src/main/java/com/leroy/oparetachallenge/utils/NetworkInterceptor.kt
+++ app/src/main/java/com/leroy/oparetachallenge/utils/NetworkInterceptor.kt
@@ -14,7 +14,7 @@ class NetworkInterceptor : Interceptor {
         val timeout =
             if (sTimeout != null && TextUtils.isDigitsOnly(sTimeout)) sTimeout.toInt() else 60
         val requestBuilder = original.newBuilder()
-            .header("X-CMC_PRO_API_KEY", "c58ec682-f50e-4fc5-91d1-b9e7e0bef66c")
+            .header("X-CMC_PRO_API_KEY", "7ef54470-ff41-462b-bab4-69b3af0c63b4")
             .method(original.method, original.body)
         val newRequest = requestBuilder.build()
         val response = chain.withConnectTimeout(timeout, TimeUnit.SECONDS).proceed(newRequest)
